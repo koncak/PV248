9e90fc3f31086d82e387f68e8225109d441d4687
david <david.koncak@seznam.cz>
2018-10-22 14:36:44 +0200

    03 fix store_score

--- 03-SQL/import.py
+++ 03-SQL/import.py
@@ -31,15 +31,19 @@ def store_voice(cursor, voice, score_id):
                    (voice.name, voice.range, voice.number, score_id))
 
 
-def store_score(cursor, score):
-    cursor.execute("SELECT * FROM score WHERE name=? AND genre=? AND key = ? AND incipit = ? AND year = ?",
+def store_score(cursor, score):  # 666,667, 694,695, 745,746
+    cursor.execute("SELECT * FROM score WHERE name=?"
+                   " AND genre=? "
+                   "AND key = ? AND "
+                   "(incipit = ? or incipit is null) AND "
+                   "(year = ? or year is null)",
                    (score.name, score.genre, score.key, score.incipit, score.year))
     stored = cursor.fetchone()
     if stored is None:
         cursor.execute("INSERT INTO score (name, genre, key, incipit, year) VALUES (?, ?, ?, ?, ?)",
                    (score.name, score.genre, score.key, score.incipit, score.year))
         return cursor.lastrowid
-    return stored.id
+    return stored[0]
 
 
 def store_author(cursor, person):
@@ -48,21 +52,33 @@ def store_author(cursor, person):
     if stored is None:
         cursor.execute("INSERT INTO person (born, died, name) VALUES (?, ?, ?)",
                        (person.born, person.died, person.name))
+        return cursor.lastrowid
     else:
         if stored[1] is None and person.born is not None:
             cursor.execute("UPDATE person SET born = ? WHERE name = ?",
                            (person.born, person.name))
+
         if stored[2] is None and person.died is not None:
             cursor.execute("UPDATE person SET died = ? WHERE name = ?",
                            (person.died, person.name))
+        return stored[0]
 
 
 def main():
     filename = sys.argv[1]
     database = sys.argv[2]
 
-    qry = open('./scorelib.sql', 'r').read()
     conn = sqlite3.connect(database)
+
+    # c=conn.cursor()
+    #
+    # c.execute(
+    #     "SELECT * FROM score where name = ? and genre = ? and key = ? and (incipit = ? or incipit is null) and (year = ? or year is null)",
+    #     ('Capriccio', 'harpsichord', 'd', None, None,))
+    #
+    # print(c.fetchall())
+
+    qry = open('./scorelib.sql', 'r').read()
 
     c = conn.cursor()
     c.executescript(qry)
@@ -74,15 +90,13 @@ def main():
         editors = []
         for author in prin.edition.authors:
             c = conn.cursor()
-            store_author(c, author)
-            editors.append(c.lastrowid)
+            editors.append(store_author(c, author))
             conn.commit()
 
         authors = []
         for author in prin.edition.composition.authors:
             c = conn.cursor()
-            store_author(c, author)
-            authors.append(c.lastrowid)
+            authors.append(store_author(c, author))
             conn.commit()
 
         c = conn.cursor()
