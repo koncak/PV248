4798e28d890c87e939a69d344b512e7bfc0af2b6
david <david.koncak@seznam.cz>
2018-11-20 16:34:11 +0100

    07 fix?

--- 07-advanced-2/music.py
+++ 07-advanced-2/music.py
@@ -1,169 +1,174 @@
-import math
-import numpy as np
 import sys
+import numpy as np
 import wave
 import struct
-from itertools import groupby
-from operator import itemgetter
-
-names = ['C', 'Cis', 'D', 'Es', 'E', 'F', 'Fis', 'G', 'Gis', 'A', 'Bes', 'B']
+from math import log2, pow
 
 
-def find_note(frequency, base):
-    c = base * pow(2, -21 / 12)
-    n = 12 * (math.log2(frequency/c))
+def output(a1, maxs_in_windows):
+    started = False
+    pitches_in_windows = []
+    for i in range(len(maxs_in_windows)):
+        clustering_pitches = {}
 
-    octave = int(n // 12)
-    pitch = int(n % 12)
+        for j in range(3):
+            if maxs_in_windows[i][j] != 0:
+                pitch, tone = frequency_to_pitch(maxs_in_windows[i][j], a1)
+                if pitch not in clustering_pitches:
+                    clustering_pitches[pitch] = tone
+                else:
+                    if clustering_pitches[pitch] < tone:
+                        clustering_pitches[pitch] = tone
 
-    cents, _ = math.modf(n)
-    cents = float("{0:.2f}".format(cents))
-    cents = int(cents * 100)
+        pitches_in_windows.append(clustering_pitches)
 
-    if cents < 0:
-        pitch += 1
+    for i in range(len(maxs_in_windows)):
+        if not started:
+            print("{0:.2f}".format(0.1 * i), "-", sep='', end='')
+            started = True
+        else:
+            if len(pitches_in_windows[i - 1]) == len(pitches_in_windows[i]):
+                contains_all = True
+                for k, v in pitches_in_windows[i - 1].items():
+                    if not (k in pitches_in_windows[i]):
+                        contains_all = False
 
-    if cents <= -50:
-        pitch -= 1
-        cents += 100
+                if contains_all:
+                    for k, v in pitches_in_windows[i - 1].items():
+                        if k in pitches_in_windows[i]:
+                            pitches_in_windows[i][k] = max(v, pitches_in_windows[i][k])
+                else:
+                    started = False
+                    print("{0:.2f}".format(0.1 * (i + 1)), sep='', end=' ')
+                    if len(pitches_in_windows[i - 1]) == 0:
+                        print("no peaks")
+                    else:
+                        for k, v in pitches_in_windows[i - 1].items():
+                            if int(v) >= 0:
+                                print(k, "+", v, sep='', end=' ')
+                            else:
+                                print(k, v, sep='', end=' ')
+                    print("\n", sep='', end='')
+
+            else:
+                started = False
+                print("{0:.2f}".format(0.1 * (i + 1)), sep='', end=' ')
+                if len(pitches_in_windows[i - 1]) == 0:
+                    print("no peaks")
+                else:
+                    for k, v in pitches_in_windows[i - 1].items():
+                        if int(v) >= 0:
+                            print(k, "+", v, sep='', end=' ')
+                        else:
+                            print(k, v, sep='', end=' ')
+                print("\n", sep='', end='')
+
+    if started:
+        print("{0:.2f}".format(0.1 * (i + 1)), sep='', end=' ')
+        if len(pitches_in_windows[i - 1]) == 0:
+            print("no peaks")
+        else:
+            for k, v in pitches_in_windows[i - 1].items():
+                print(k, "+", v, sep='', end=' ')
+        print("\n", sep='', end='')
+
+
+def Nmaxelements(list1, n):
+    final_list = []
+
+    for i in range(0, n):
+        max1 = 0
+        index = 0
+
+        for j in range(len(list1)):
+            if list1[j] > max1:
+                max1 = list1[j]
+                index = j
+
+        list1[index] = 0
+        final_list.append((max1, index))
+
+    return final_list
+
+
+def frequency_to_pitch(f, a1):
+    pitches = ['c', 'cis', 'd', 'es', 'e', 'f', 'fis', 'g', 'gis', 'a', 'bes', 'b']
+    middleC = a1 * pow(2, (-21 / 12)) * 2
+    h = 12 * log2(f / middleC) #/ log2(2)
+
+    octave_index = int(h // 12) + 1
+    index = int(h % 12)
+    cents = int((h % 1) * 100)
 
     if cents >= 50:
-        pitch += 1
+        index += 1
         cents = cents - 100
 
-    if pitch >= 12:
-        pitch -= 12
-        octave += 1
+    if index >= 12:
+        index -= 12
+        octave_index += 1
 
-    out = names[pitch]
+    pitch = pitches[index]
+    octave_char = "’"
+    if octave_index < 0:
+        pitch = pitch.capitalize()
+        octave_char = ","
 
-    if octave >= 0:
-        out = out.lower() + ("’" * octave)
+    if octave_index < 0:
+        octave_index += 1
 
-    else:
-        octave += 1
-        out = out + (',' * -octave)
+    pitch = pitch + np.abs(octave_index) * octave_char
 
-    return out, cents
-
-
-def join_intervals(intervals, base):
-    unique_peaks = set([x[0] for x in intervals])
-    all_peaks = []
-
-    for peak in unique_peaks:
-        occurrence = [x[1] for x in intervals if x[0] == peak]
-
-        for k, g in groupby(enumerate(occurrence), lambda ix: ix[0] - ix[1]):
-            ranges = list(map(itemgetter(1), g))
-
-            note = find_note(peak, base)
-            if note[1] < 0:
-                cents = "-"
-            else:
-                cents = "+"
-
-            note_out = str(note[0] + cents + str(abs(note[1])))
-            all_peaks.append((note_out, (ranges[0]/10, (ranges[-1]+1)/10), peak))
-
-    return all_peaks
+    return str(pitch), str(cents)
 
 
 def main():
-    # 0.0 - 0.7 C - 37 C + 16 g + 0
-    # 0.0 - 2.3 g + 0 ------ ???
-    # 0.7 - 1.1 C - 10 C + 42 g + 0
-    # 1.1 - 1.9 C - 37 C + 16 g + 0
-    # 1.9 - 2.3 C - 10 C + 42 g + 0
-
-    base = int(sys.argv[1])
+    a1 = float(sys.argv[1])
     filename = sys.argv[2]
-
     wav = wave.open(filename, 'r')
 
-    width = wav.getsampwidth()
-    frames = wav.getnframes()
-    channels = wav.getnchannels()
-    samples = frames * channels
-
-    if width == 2:
-        fmt = str(samples) + "h"
-    else:
-        raise ValueError("16 bit formats only.")
-        wav.close()
-        return -1
-
+    num_channels = wav.getnchannels()
     frame_rate = wav.getframerate()
-    bytes = wav.readframes(wav.getnframes())
-    integers = struct.unpack(fmt, bytes)
-    step = frame_rate // 10
+    frames = wav.getnframes()
+    raw_data = wav.readframes(frames)
 
-    if channels == 2:
+    fmt = str(frames*num_channels) + "h"
+    integers = struct.unpack(fmt, raw_data)
+    integers = np.array(integers)
+
+    if num_channels == 2:
         integers = [(a + b) / 2 for a, b in zip(integers[::2], integers[1::2])]
 
-    out = []
-    position = 0
+    step = frame_rate // 10
+    maxs_in_windows = []
     for i in range(0, (len(integers)-frame_rate), step):
 
-        fft = np.fft.rfft(integers[i:frame_rate + i])
-        mean = np.mean([np.abs(x) for x in fft])
+        fft_data = np.fft.rfft(integers[i:frame_rate+i])
 
-        peaks = []
-        for j in range(len(fft)):
-            freq = np.abs(fft[j])
-            if mean*20 <= freq:
-                peaks.append((freq, j))
+        fft_data = np.abs(fft_data)
+        peak = 20 * sum(fft_data) / len(fft_data)
+
+        peaks = [fft_data[i] if fft_data[i] > peak else 0 for i in range(len(fft_data))]
+
+        clusters = []
+        for i in range(1, len(peaks)):
+            if peaks[i-1] > peaks[i]:
+                if len(clusters) > 0:
+                    if clusters[-1] == 0:
+                        clusters.append(peaks[i-1])
+                    else:
+                        clusters.append(0)
+                else:
+                    clusters.append(0)
             else:
-                peaks.append((-1, -1))
+                clusters.append(0)
 
-        local_out = []
-        for _ in range(3):
-            maximum = max(peaks, key=lambda item: item[0])
+        window_maxs = [x[1] for x in Nmaxelements(clusters, 3)]
+        window_maxs.sort()
+        maxs_in_windows.append(window_maxs)
 
-            index = peaks.index(maximum)
-            left = index - 1
-            right = index + 2
-            if index == 0:
-                left = index
-            if index == len(peaks):
-                right = index + 1
-
-            del peaks[left:right]
-            if maximum != (-1, -1):
-                local_out.append((maximum[1],))
-
-        for x in local_out:
-            out.append(x+(position,))
-        position += 1
-
-    all_peaks = join_intervals(out, base)
-    if len(all_peaks) == 0:
-        print("no peaks")
-        return
-
-    interval_tuples = list(set([x[1] for x in all_peaks]))
-    interval_tuples = sorted(interval_tuples)
-
-    for interval in interval_tuples:
-        tones = []
-        for peak in all_peaks:
-            if peak[1][0] <= interval[0] and peak[1][1] >= interval[1]:
-                tones.append((peak[0], peak[2]))
-
-        tones.sort(key=lambda tup: tup[1])
-        out = []
-        for tone in tones:
-            if tone[0] not in [o[0] for o in out]:
-                out.append(tone)
-        out.sort(key=lambda tup: tup[1])
-
-        print(interval[0], "-", interval[1], end=' ')
-        for tone in out:
-            print(tone[0], end=' ')
-        print()
+    output(a1, maxs_in_windows)
 
     wav.close()
-    return
-
 
 main()
