b039a0a58ba9eb60551bc1ed3085b261906a786d
david <david.koncak@seznam.cz>
2019-01-21 12:02:09 +0100

    03 fix

--- 03-SQL/scorelib.py
+++ 03-SQL/scorelib.py
@@ -1,158 +1,205 @@
+import os
+import re
+import sys
+import operator
+
 
 class Print:
-
-    def __init__(self, edition, print_id, partiture):
-        self.edition = edition
-        self.print_id = print_id
-        self.partiture = partiture
+    def __init__(self):
+        self.edition = Edition()
+        self.print_id = None
+        self.partiture = None
 
     def format(self):
-        print("Print Number:", self.print_id)
+        partiture = ""
+        if (self.partiture):
+            partiture = "yes"
+        if(not self.partiture):
+            partiture = "no"
 
-        self.print_persons("composer")
+        edition = self.edition.toString()
+        if(edition.strip()):
+            edition = "\n" + edition
 
-        print("Title:", end='')
-        if self.edition.composition.name:
-            print("", self.edition.composition.name)
-        else:
-            print()
-
-        print("Genre:", end='')
-        if self.edition.composition.genre:
-            print("", self.edition.composition.genre)
-        else:
-            print()
-
-        print("Key:", end='')
-        if self.edition.composition.key:
-            print("", self.edition.composition.key)
-        else:
-            print()
-
-        print("Composition Year:", end='')
-        if self.edition.composition.year:
-            print("", self.edition.composition.year)
-        else:
-            print()
-
-        print("Edition:", end='')
-        if self.edition.name:
-            print("", self.edition.name)
-        else:
-            print()
-
-        self.print_persons("editor")
-
-        voices = self.edition.composition.voices
-        if voices:
-            for i in range(len(voices)):
-                print("Voice " + str(i+1) + ": ", end='')
-                if voices[i].range:
-                    print(voices[i].range, end='')
-                    if voices[i].name:
-                        print(",", voices[i].name)
-                    else:
-                        print()
-                if not voices[i].range and voices[i].name:
-                    print(voices[i].name)
-
-        else:
-            print("Voice 1:")
-
-        print("Partiture:", "yes" if self.partiture else "no")
-
-        print("Incipit:", end='')
-        if self.edition.composition.incipit:
-            print("", self.edition.composition.incipit)
-        else:
-            print()
-
-        print()
+        print("Print Number: {}{}\nPartiture: {}\n".format(
+              self.print_id,
+              edition,
+              partiture))
 
     def composition(self):
         return self.edition.composition
 
-    def print_persons(self, type):
-        if type == "composer":
-            out_string = "Composer:"
-            authors = self.edition.composition.authors
-
-        if type == "editor":
-            out_string = "Editor:"
-            authors = self.edition.authors
-
-        if len(authors) > 0:
-            for i in range(len(authors)):
-                author = authors[i]
-                if i == 0:
-                    print(out_string+" ", end='')
-                    print(author.name, end='')
-                    if author.born is not None:
-                        print(' ({}--'.format(author.born), end='')
-                        if author.died is None:
-                            print(')', end='')
-
-                    if author.died is not None:
-                        if author.born is None:
-                            print('(--', end='')
-                        print('{})'.format(author.died), end='')
-                else:
-                    print("; ", author.name, end='')
-                    if author.born is not None:
-                        print('({}--'.format(author.born), end='')
-                    if author.died is not None:
-                        print('{})'.format(author.died), end='')
-            print()
-        else:
-            print(out_string)
+    def setBooleanByString(self, boolString):
+        if(boolString == "no"):
+            self.partiture = False
+        elif(boolString == "yes"):
+            self.partiture = True
 
 
 class Edition:
+    def __init__(self):
+        self.composition = Composition()
+        self.authors = list()
+        self.name = None
+        self.year = None
 
-    def __init__(self, composition, name):
-        self.composition = composition
-        self.authors = []
-        self.name = name
+    def setEditorsByString(self, editors):
+        self.authors.extend(map(getPersonFromString, editors.split(", ")))
 
-    def add_author(self, author):
-        self.authors.append(author)
+    def toString(self):
+        return "Edition: {}\nEditor: {}\nPublication Year: {}\n{}".format(
+            self.name or "",
+            ", ".join(map(lambda x: x.toString(), self.authors)),
+            self.year or "",
+            self.composition.toString())
 
 
 class Composition:
+    def __init__(self):
+        self.name = None
+        self.incipit = None
+        self.key = None
+        self.genre = None
+        self.year = None
+        self.voices = list()
+        self.authors = list()
 
-    def __init__(self, name, incipit, key, genre, year):
-        self.name = name
-        self.incipit = incipit
-        self.key = key
-        self.genre = genre
-        self.year = year
-        self.voices = []
-        self.authors = []
+    def toString(self):
+        voices = "\n".join(map(lambda x: x.toString(), self.voices))
+        if(voices.strip()):
+            voices = "\n" + voices
+        return "Title: {}\nIncipit: {}\nKey: {}\nGenre: {}\nComposition Year: {}\nComposer: {}{}".format(
+            self.name or "",
+            self.incipit or "",
+            self.key or "",
+            self.genre or "",
+            self.year or "",
+            "; ".join(map(lambda x: x.toString(), self.authors)),
+            voices
+        )
 
-    def add_author(self, author):
-        self.authors.append(author)
+    def addComposersByString(self, names):
+        self.authors.extend(map(getPersonFromString, names.split("; ")))
 
-    def add_voice(self, voice):
-        self.voices.append(voice)
+    def addVoiceByString(self, key, voice):
+        v = Voice()
+        v.setVoiceByString(key, voice)
+        self.voices.append(v)
 
 
 class Voice:
+    def __init__(self):
+        self.order = 0
+        self.name = None
+        self.range = None
 
-    def __init__(self, name, range):
-        self.name = name
-        self.range = range
-        self.number = None
+    def setVoiceByString(self, key, voice):
+        rKey = re.match(r"voice (\d*)", key)
+        rVoice = re.match(r"(.*?--.*?)[,|;] (.*)|(.*)", voice)
+        self.order = int(rKey.group(1))
 
-    def check_range(self):
-        if self.range and self.range.count("-") == 1:
-            position = self.range.find("-")
-            self.range = self.range[:position] + "-" + self.range[position:]
+        if(not rVoice.group(1)):
+            self.name = voice
+        else:
+            self.name = rVoice.group(2)
+            self.range = rVoice.group(1)
+
+    def toString(self):
+        if(self.range is None):
+            return "Voice {}: {}".format(self.order, self.name)
+        return "Voice {}: {}, {}".format(self.order, self.range, self.name)
 
 
 class Person:
+    def __init__(self):
+        self.name = None
+        self.born = None
+        self.died = None
 
-    def __init__(self, name, born, died):
-        self.name = name
-        self.born = born
-        self.died = died
+    def toString(self):
+        if(self.born is not None or self.died is not None):
+            return "{} ({}--{})".format(self.name or "", self.born or "", self.died or "")
+        return self.name or ""
 
+
+def load(filename):
+    content = open(filename, 'r', encoding="utf-8").read()
+
+    if not content:
+        return ""
+
+    blockObjects = content.split("\n\n")
+    result = list()
+
+    for blockObject in blockObjects:
+        print = Print()
+
+        for key, value in filter(None, map(splitKeyAndValue, blockObject.split("\n"))):
+            if key is None or value is None:
+                continue
+
+            key = key.lower()
+            if(key == "print number"):
+                print.print_id = int(value)
+            elif(key == "composer"):
+                print.composition().addComposersByString(value)
+            elif(key == "title"):
+                print.composition().name = value.strip()
+            elif(key == "genre"):
+                print.composition().genre = value
+            elif(key == "key"):
+                print.composition().key = value
+            elif(key == "composition year"):
+                value = value.strip()
+                if(value.isdigit()):
+                    print.composition().year = int(value)
+            elif(key == "publication year"):
+                value = value.strip()
+                if(value.isdigit()):
+                    print.edition.year = int(value)
+            elif(key == "edition"):
+                print.edition.name = value.strip()
+            elif(key == "editor"):
+                print.edition.setEditorsByString(value)
+            elif(key == "partiture"):
+                print.setBooleanByString(value)
+            elif(key == "incipit"):
+                print.composition().incipit = value
+            elif(str(key).startswith("voice")):
+                print.composition().addVoiceByString(key, value)
+
+        if(print.print_id is not None):
+            result.append(print)
+
+    return sorted(result, key=operator.attrgetter('print_id'))
+
+
+def getPersonFromString(person):
+    r = re.match(
+        r"(.*) \(((\d*)--(\d*)|\+(\d+)|\*(\d+)|(\d+))\)", person)
+    someone = Person()
+    if(r is None):
+        someone.name = person
+    else:
+        someone.name = r.group(1)
+        if(r.group(3)):
+            someone.born = int(r.group(3))
+        elif(r.group(6)):
+            someone.born = int(r.group(6))
+        elif(r.group(7)):
+            someone.born = int(r.group(7))
+
+        if(r.group(4)):
+            someone.died = int(r.group(4))
+        elif(r.group(5)):
+            someone.died = int(r.group(5))
+    return someone
+
+
+def splitKeyAndValue(line):
+    r = re.compile("(.*): (.*)")
+    result = r.match(line)
+
+    if(result is None or not result.group(2)):
+        return None, None
+    return (result.group(1), result.group(2).strip())
